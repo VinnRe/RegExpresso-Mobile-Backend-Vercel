16b82f3837849424fd5e258832e94543265c5876
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-12-01 10:29:11 +0800

    Add test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/parse", parseRoutes);
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send('WORKING!');
 });
 
